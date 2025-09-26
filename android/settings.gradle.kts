971710b9c65cc351c0149805934d765af7d2f6c7
sirana236-web <sirana236@gmail.com>
2025-09-26 20:12:25 +0330

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,2 +1,16 @@
-// فایل android/settings.gradle.kts - کوتاه‌ترین نسخه
 include(":app")
+
+val flutterProjectRoot = rootProject.projectDir.parentFile.toPath()
+
+val localPropertiesFile = File(rootProject.projectDir, "local.properties")
+if (localPropertiesFile.exists()) {
+    val properties = java.util.Properties()
+    localPropertiesFile.inputStream().use { properties.load(it) }
+    
+    val flutterSdkPath = properties.getProperty("flutter.sdk")
+    if (flutterSdkPath != null) {
+        apply {
+            from("$flutterSdkPath/packages/flutter_tools/gradle/app_plugin_loader.gradle")
+        }
+    }
+}
